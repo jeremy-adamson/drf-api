4123db6300517319a16dd5232b3f4f1ca8ffe41e
jeremy-adamson <jadamson.physics@gmail.com>
2023-08-15 04:59:16 +0000

    lab32 test

--- more_things_api/more_things_api/settings.py
+++ more_things_api/more_things_api/settings.py
@@ -87,6 +87,17 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#         "NAME": "postgres",
+#         "USER": "postgres",
+#         "PASSWORD": "postgres",
+#         "HOST": "db",
+#         "PORT": 5432,
+#     }
+# }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -131,6 +142,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CALSSES':[
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenicated',
     ]
 }
